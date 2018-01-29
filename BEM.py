f65f0768d9ff762967f7897bee641f0195b48f91
Rohit Ner <rohitner1@gmail.com>
2018-01-29 17:19:15 +0530

    updated gif

--- BEM.py
+++ BEM.py
@@ -13,35 +13,40 @@ y0 = 1
 lower0 = 0
 upper0 = 1
 
+
 def analytic(x):
-    return -2 * ( x + 1 ) + 3 * np.e ** x
+    return -2 * (x + 1) + 3 * np.e ** x
+
 
 def f(x, y):
     return 2 * x + y
 
-def BEM(h,y,lower,upper):
-    interval = np.arange(lower,upper+h,h)
+
+def BEM(h, y, lower, upper):
+    interval = np.arange(lower + h, upper + h, h)
     ndsolve = np.array([y])
     for x in interval:
-        # y1 = y0 + h * f(x0, y1)
-        #    = y0 + h * ( 2 * x0 + y1 )
-        # y1 = ( y0 + 2 * h * x0 )/( 1 - h )
-        y = ( y + 2 * h * x )/( 1 - h )
-        ndsolve = np.append(ndsolve,y)
+        # y1 = y0 + h * f(x1, y1)
+        #    = y0 + h * ( 2 * x1 + y1 )
+        # y1 = ( y0 + 2 * h * x1 )/( 1 - h )
+        y = (y + 2 * h * x)/(1 - h)
+        ndsolve = np.append(ndsolve, y)
     ndsolve = np.delete(ndsolve, -1)
+    interval = np.arange(lower, upper, h)
     return interval, ndsolve
 
-interval0, ndsolve0 = BEM(h0,y0,lower0,upper0)
-l, = plt.plot(interval0,ndsolve0,'rx')
-plt.plot(np.arange(0,1,0.001),analytic(np.arange(0,1,0.001)),'--')
+interval0, ndsolve0 = BEM(h0, y0, lower0, upper0)
+l, = plt.plot(interval0, ndsolve0, 'rx')
+plt.plot(np.arange(0, 1, 0.001), analytic(np.arange(0, 1, 0.001)), '--')
 
 axcolor = 'lightgoldenrodyellow'
 axh = plt.axes([0.25, 0.1, 0.5, 0.1], facecolor=axcolor)
 sh = Slider(axh, 'h', 0.001, 1., valinit=0.2)
 
+
 def update(val):
     h0 = sh.val
-    interval0, ndsolve0 = BEM(h0,y0,lower0,upper0)
+    interval0, ndsolve0 = BEM(h0, y0, lower0, upper0)
     l.set_data(interval0, ndsolve0)
     fig.canvas.draw_idle()
 
--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # MA20102
 Numerical Solution of Ordinary and Partial Differential Equation
 ________________________
-![FEM](/FEM.gif) ![FEM](/BEM.gif)
+##### Forward Euler Method
+![FEM](/FEM.gif)
+
+##### Backward Euler Method
+![FEM](/BEM.gif)
+________________________
+### Usage
+```
+python [filename.py]
+```
+Use the slider for refining the plot.
+
+### Dependencies
+- Matplotlib
+- Numpy
+
+### License
+GNU GPL
